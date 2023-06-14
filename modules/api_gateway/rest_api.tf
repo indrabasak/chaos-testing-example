a6aac88ff25425b588b074c85d04c140cad7835c
basaki <indra.basak@autodesk.com>
2023-06-14 15:32:21 -0700

    working localstack

--- modules/api_gateway/rest_api.tf
+++ modules/api_gateway/rest_api.tf
@@ -64,12 +64,11 @@ resource "aws_api_gateway_stage" "rest_api_stage" {
   stage_name    = var.rest_api_stage_name
 }
 
-data "aws_api_gateway_export" "rest_api_gateway_export" {
-  rest_api_id = aws_api_gateway_stage.rest_api_stage.rest_api_id
-  stage_name  = aws_api_gateway_stage.rest_api_stage.stage_name
-#  rest_api_root_resource_id = aws_api_gateway_rest_api.rest_api.root_resource_id
-  export_type = "oas30"
-}
+#data "aws_api_gateway_export" "rest_api_gateway_export" {
+#  rest_api_id = aws_api_gateway_stage.rest_api_stage.rest_api_id
+#  stage_name  = aws_api_gateway_stage.rest_api_stage.stage_name
+#  export_type = "oas30"
+#}
 
 resource "aws_ssm_parameter" "ssm_parameter_rest_api_id" {
   name        = "/chaos-testing-example/rest_api_id"
--- new-setup.sh
+++ new-setup.sh
@@ -6,7 +6,7 @@ TARGET_ENVIRONMENT="sbx"
 BUCKET_NAME="sbx-chaos-testing-example-state-us-west-2"
 
 usage() {
-    echo "<deploy|destroy>"
+    echo "<deploy|destroy|test>"
 }
 
 terraformDeploy() {
@@ -49,13 +49,59 @@ terraformDestroy() {
 }
 
 serverlessDeploy() {
-  echo "serverlessDeploy - 1"
-
+  echo "serverlessDeploy - started"
   sls deploy --stage ${TARGET_ENVIRONMENT} --region ${TARGET_REGION}
+  echo "serverlessDeploy - completed"
 }
 
-# terraformDeploy
-# terraformDestroy
+serverlessDestroy() {
+  echo "serverlessDestroy - started"
+  sls remove --stage  ${TARGET_ENVIRONMENT} --region ${TARGET_REGION}
+  echo "serverlessDestroy - completed"
+}
+
+startLocalstack() {
+#  dockerStatus=$(docker-compose up)
+  echo "1 -------------"
+  echo ${dockerStatus}
+  echo "2 -------------"
+  localstack start
+}
+
+terraformDeployLocal() {
+    echo "terraformDeploy - 1"
+    export TF_LOG="DEBUG"
+
+    tflocal init
+
+    bucketImportStatus=$(tflocal import -var "key=${BASE_NAME}/terraform.tfstate" -var-file=./environments/${TARGET_REGION}/${TARGET_ENVIRONMENT}.tfvars aws_s3_bucket_public_access_block.terraform_state ${BUCKET_NAME})
+    echo "------------------"
+    echo $bucketImportStatus
+    bucketPublicAccessBlockImportStatus=$(tflocal import -var "key=${BASE_NAME}/terraform.tfstate" -var-file=./environments/${TARGET_REGION}/${TARGET_ENVIRONMENT}.tfvars aws_s3_bucket_public_access_block.terraform_state ${BUCKET_NAME})
+    echo "------------------"
+    echo $bucketPublicAccessBlockImportStatus
+
+    if [[ -n $bucketImportStatus || -n $bucketPublicAccessBlockImportStatus ]]; then
+      echo "The terraform S3 backend could not be imported, so one will now be created."
+
+      terraformPlanOutputFile="$(pwd)/out.plan"
+      echo "%%%%%%%%%%%%%%%%%%%%%"
+      echo ${terraformPlanOutputFile}
+      echo "1 ------------------"
+      tflocal plan -var "key=${BASE_NAME}/terraform.tfstate" -var-file=./environments/${TARGET_REGION}/${TARGET_ENVIRONMENT}.tfvars -out="${terraformPlanOutputFile}"
+      echo "2 ------------------"
+      tflocal apply -auto-approve -lock=true "${terraformPlanOutputFile}"
+    else
+      echo "An existing remote backend has been detected successfully."
+      echo "Re-initializing using the detected remote backend."
+    fi
+}
+
+serverlessDeployLocal() {
+  echo "serverlessDeployLocal - started"
+  sls deploy --stage local --region ${TARGET_REGION}
+  echo "serverlessDeployLocal - completed"
+}
 
 if [ "$#" -ne 1 ]; then
     echo "Improper Arguments passed"
@@ -68,14 +114,23 @@ fi
 #REG_SEC="us-west-2"
 
 if [ "$1" = "deploy" ]; then
-    echo "deploy"
+    echo "deployment started"
     terraformDeploy
     serverlessDeploy
+    echo "deployment completed"
 elif [ "$1" = "destroy" ]; then
-    echo "destroy"
+    echo "destruction started"
+    serverlessDestroy
     terraformDestroy
+    echo "destruction completed"
+elif [ "$1" = "test" ]; then
+    echo "test started"
+    startLocalstack
+    terraformDeployLocal
+    serverlessDeployLocal
+    echo "test completed"
 else
-    echo "Improper Arguments passed"
+    echo "Incorrect arguments passed"
     usage
 fi
 
