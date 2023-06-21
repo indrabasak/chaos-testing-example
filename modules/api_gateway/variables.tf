7947d436667451aa42012aeec85d5b217b49060d
basaki <indra.basak@autodesk.com>
2023-06-20 23:02:15 -0700

    cleanup

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -13,13 +13,11 @@ variable "rest_api_stage_name" {
 variable "root_domain" {
   type        = string
   description = "The domain name to associate with the API"
-  #  default     = "demo.hands-on-cloud.com" //replace your root domain name here
   default = "demo.com"
 }
 
 variable "subdomain"{
   type        = string
   description = "The subdomain for the API"
-  #  default     = "api.demo.hands-on-cloud.com" //replace your subdomain name here
   default = "chaos-example.demo.com"
 }
